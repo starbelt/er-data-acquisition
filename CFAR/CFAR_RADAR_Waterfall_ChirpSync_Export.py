79b57f1d2ced45986159304b81471f77afe07219
Nathan Griffin <nathangriffin@vt.edu>
2025-02-24 12:58:52 -0500

    added type ingnores

--- CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
+++ CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
@@ -18,17 +18,17 @@
 import sys
 import time
 import numpy as np
-import pyqtgraph as pg
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
-from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph as pg  # type: ignore[all]
+from PyQt5.QtCore import Qt # type: ignore
+from PyQt5.QtWidgets import * # type: ignore
+from pyqtgraph.Qt import QtCore, QtGui # type: ignore
 from target_detection_dbfs import cfar
 import csv
 import datetime
 import os
-import adi
+import adi # type: ignore
 from collections import defaultdict
-import cv2
+import cv2 # type: ignore
 
 '''Key Parameters'''
 sample_rate = 0.682e6
@@ -241,7 +241,7 @@ my_sdr.tx([iq, iq])
 """
 plot_threshold = False
 cfar_toggle = False
-class Window(QMainWindow):
+class Window(QMainWindow): # type: ignore
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Interactive FFT")
@@ -255,13 +255,13 @@ class Window(QMainWindow):
 
     # method for components
     def UiComponents(self):
-        widget = QWidget()
+        widget = QWidget() # type: ignore[all]
 
         global layout, signal_freq, plot_freq
-        layout = QGridLayout()
+        layout = QGridLayout() # type: ignore[all]
 
         # Control Panel
-        control_label = QLabel("PHASER CFAR Targeting")
+        control_label = QLabel("PHASER CFAR Targeting") # type: ignore[all]
         font = control_label.font()
         font.setPointSize(24)
         control_label.setFont(font)
@@ -270,82 +270,82 @@ class Window(QMainWindow):
         layout.addWidget(control_label, 0, 0, 1, 2)
 
         # Check boxes
-        self.thresh_check = QCheckBox("Plot CFAR Threshold")
+        self.thresh_check = QCheckBox("Plot CFAR Threshold")  # type: ignore[all]
         font = self.thresh_check.font()
         font.setPointSize(10)
         self.thresh_check.setFont(font)
         self.thresh_check.stateChanged.connect(self.change_thresh)
         layout.addWidget(self.thresh_check, 2, 0)
         
-        self.cfar_check = QCheckBox("Apply CFAR Threshold")
+        self.cfar_check = QCheckBox("Apply CFAR Threshold") # type: ignore[all]
         font = self.cfar_check.font()
         self.cfar_check.setFont(font)
         self.cfar_check.stateChanged.connect(self.change_cfar)
         layout.addWidget(self.cfar_check, 2, 1)
 
         # Chirp bandwidth slider
-        self.bw_slider = QSlider(Qt.Horizontal)
+        self.bw_slider = QSlider(Qt.Horizontal)  # type: ignore[all]
         self.bw_slider.setMinimum(100)
         self.bw_slider.setMaximum(500)
         self.bw_slider.setValue(int(default_chirp_bw / 1e6))
         self.bw_slider.setTickInterval(50)
         self.bw_slider.setMaximumWidth(200)
-        self.bw_slider.setTickPosition(QSlider.TicksBelow)
+        self.bw_slider.setTickPosition(QSlider.TicksBelow)  # type: ignore[all]
         self.bw_slider.valueChanged.connect(self.get_range_res)
         layout.addWidget(self.bw_slider, 4, 0)
 
-        self.set_bw = QPushButton("Set Chirp Bandwidth")
+        self.set_bw = QPushButton("Set Chirp Bandwidth") # type: ignore[all]
         self.set_bw.setMaximumWidth(200)
         self.set_bw.pressed.connect(self.set_range_res)
         layout.addWidget(self.set_bw, 5, 0, 1, 1)
         
-        self.quit_button = QPushButton("Quit")
+        self.quit_button = QPushButton("Quit") # type: ignore[all]
         self.quit_button.pressed.connect(self.end_program)
         layout.addWidget(self.quit_button, 30, 0, 4, 4)
         
         #CFAR Sliders
-        self.cfar_bias = QSlider(Qt.Horizontal)
+        self.cfar_bias = QSlider(Qt.Horizontal) # type: ignore[all]
         self.cfar_bias.setMinimum(0)
         self.cfar_bias.setMaximum(100)
         self.cfar_bias.setValue(25)
         self.cfar_bias.setTickInterval(5)
         self.cfar_bias.setMaximumWidth(200)
-        self.cfar_bias.setTickPosition(QSlider.TicksBelow)
+        self.cfar_bias.setTickPosition(QSlider.TicksBelow) # type: ignore[all]
         self.cfar_bias.valueChanged.connect(self.get_cfar_values)
         layout.addWidget(self.cfar_bias, 8, 0)
-        self.cfar_bias_label = QLabel("CFAR Bias (dB): %0.0f" % (self.cfar_bias.value()))
+        self.cfar_bias_label = QLabel("CFAR Bias (dB): %0.0f" % (self.cfar_bias.value())) # type: ignore[all]
         self.cfar_bias_label.setFont(font)
         self.cfar_bias_label.setAlignment(Qt.AlignLeft)
         self.cfar_bias_label.setMinimumWidth(100)
         self.cfar_bias_label.setMaximumWidth(200)
         layout.addWidget(self.cfar_bias_label, 8, 1)
         
-        self.cfar_guard = QSlider(Qt.Horizontal)
+        self.cfar_guard = QSlider(Qt.Horizontal)  # type: ignore[all]
         self.cfar_guard.setMinimum(1)
         self.cfar_guard.setMaximum(40)
         self.cfar_guard.setValue(15)
         self.cfar_guard.setTickInterval(4)
         self.cfar_guard.setMaximumWidth(200)
-        self.cfar_guard.setTickPosition(QSlider.TicksBelow)
+        self.cfar_guard.setTickPosition(QSlider.TicksBelow)# type: ignore[all]
         self.cfar_guard.valueChanged.connect(self.get_cfar_values)
         layout.addWidget(self.cfar_guard, 10, 0)
-        self.cfar_guard_label = QLabel("Num Guard Cells: %0.0f" % (self.cfar_guard.value()))
+        self.cfar_guard_label = QLabel("Num Guard Cells: %0.0f" % (self.cfar_guard.value()))# type: ignore[all]
         self.cfar_guard_label.setFont(font)
         self.cfar_guard_label.setAlignment(Qt.AlignLeft)
         self.cfar_guard_label.setMinimumWidth(100)
         self.cfar_guard_label.setMaximumWidth(200)
         layout.addWidget(self.cfar_guard_label, 10, 1)
         
-        self.cfar_ref = QSlider(Qt.Horizontal)
+        self.cfar_ref = QSlider(Qt.Horizontal)# type: ignore[all]
         self.cfar_ref.setMinimum(1)
         self.cfar_ref.setMaximum(100)
         self.cfar_ref.setValue(16)
         self.cfar_ref.setTickInterval(10)
         self.cfar_ref.setMaximumWidth(200)
-        self.cfar_ref.setTickPosition(QSlider.TicksBelow)
+        self.cfar_ref.setTickPosition(QSlider.TicksBelow)# type: ignore[all]
         self.cfar_ref.valueChanged.connect(self.get_cfar_values)
         layout.addWidget(self.cfar_ref, 12, 0)
-        self.cfar_ref_label = QLabel("Num Ref Cells: %0.0f" % (self.cfar_ref.value()))
+        self.cfar_ref_label = QLabel("Num Ref Cells: %0.0f" % (self.cfar_ref.value()))# type: ignore[all]
         self.cfar_ref_label.setFont(font)
         self.cfar_ref_label.setAlignment(Qt.AlignLeft)
         self.cfar_ref_label.setMinimumWidth(100)
@@ -353,61 +353,61 @@ class Window(QMainWindow):
         layout.addWidget(self.cfar_ref_label, 12, 1)
 
         # waterfall level slider
-        self.low_slider = QSlider(Qt.Horizontal)
+        self.low_slider = QSlider(Qt.Horizontal)# type: ignore[all]
         self.low_slider.setMinimum(-100)
         self.low_slider.setMaximum(20)
         self.low_slider.setValue(-100)
         self.low_slider.setTickInterval(5)
         self.low_slider.setMaximumWidth(200)
-        self.low_slider.setTickPosition(QSlider.TicksBelow)
+        self.low_slider.setTickPosition(QSlider.TicksBelow)# type: ignore[all]
         self.low_slider.valueChanged.connect(self.get_water_levels)
         layout.addWidget(self.low_slider, 16, 0)
 
-        self.high_slider = QSlider(Qt.Horizontal)
+        self.high_slider = QSlider(Qt.Horizontal)# type: ignore[all]
         self.high_slider.setMinimum(-100)
         self.high_slider.setMaximum(20)
         self.high_slider.setValue(20)
         self.high_slider.setTickInterval(5)
         self.high_slider.setMaximumWidth(200)
-        self.high_slider.setTickPosition(QSlider.TicksBelow)
+        self.high_slider.setTickPosition(QSlider.TicksBelow)# type: ignore[all]
         self.high_slider.valueChanged.connect(self.get_water_levels)
         layout.addWidget(self.high_slider, 18, 0)
 
-        self.water_label = QLabel("Waterfall Intensity Levels")
+        self.water_label = QLabel("Waterfall Intensity Levels")# type: ignore[all]
         self.water_label.setFont(font)
         self.water_label.setAlignment(Qt.AlignCenter)
         self.water_label.setMinimumWidth(100)
         self.water_label.setMaximumWidth(200)
         layout.addWidget(self.water_label, 15, 0,1,1)
-        self.low_label = QLabel("LOW LEVEL: %0.0f" % (self.low_slider.value()))
+        self.low_label = QLabel("LOW LEVEL: %0.0f" % (self.low_slider.value()))# type: ignore[all]
         self.low_label.setFont(font)
         self.low_label.setAlignment(Qt.AlignLeft)
         self.low_label.setMinimumWidth(100)
         self.low_label.setMaximumWidth(200)
         layout.addWidget(self.low_label, 16, 1)
-        self.high_label = QLabel("HIGH LEVEL: %0.0f" % (self.high_slider.value()))
+        self.high_label = QLabel("HIGH LEVEL: %0.0f" % (self.high_slider.value()))# type: ignore[all]
         self.high_label.setFont(font)
         self.high_label.setAlignment(Qt.AlignLeft)
         self.high_label.setMinimumWidth(100)
         self.high_label.setMaximumWidth(200)
         layout.addWidget(self.high_label, 18, 1)
 
-        self.steer_slider = QSlider(Qt.Horizontal)
+        self.steer_slider = QSlider(Qt.Horizontal)# type: ignore[all]
         self.steer_slider.setMinimum(-80)
         self.steer_slider.setMaximum(80)
         self.steer_slider.setValue(0)
         self.steer_slider.setTickInterval(20)
         self.steer_slider.setMaximumWidth(200)
-        self.steer_slider.setTickPosition(QSlider.TicksBelow)
+        self.steer_slider.setTickPosition(QSlider.TicksBelow)# type: ignore[all]
         self.steer_slider.valueChanged.connect(self.get_steer_angle)
         layout.addWidget(self.steer_slider, 22, 0)
-        self.steer_title = QLabel("Receive Steering Angle")
+        self.steer_title = QLabel("Receive Steering Angle")# type: ignore[all]
         self.steer_title.setFont(font)
         self.steer_title.setAlignment(Qt.AlignCenter)
         self.steer_title.setMinimumWidth(100)
         self.steer_title.setMaximumWidth(200)
         layout.addWidget(self.steer_title, 21, 0)
-        self.steer_label = QLabel("%0.0f DEG" % (self.steer_slider.value()))
+        self.steer_label = QLabel("%0.0f DEG" % (self.steer_slider.value()))# type: ignore[all]
         self.steer_label.setFont(font)
         self.steer_label.setAlignment(Qt.AlignLeft)
         self.steer_label.setMinimumWidth(100)
@@ -547,7 +547,7 @@ class Window(QMainWindow):
             cfar_toggle = False
 
 # create pyqt5 app
-App = QApplication(sys.argv)
+App = QApplication(sys.argv) # type: ignore[all]
 
 # create the instance of our Window
 win = Window()
