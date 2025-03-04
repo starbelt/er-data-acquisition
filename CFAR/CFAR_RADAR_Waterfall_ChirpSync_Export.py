0b097037fe6fcda0912b7e4b9366adc1232b5eb7
Nathan Griffin <nathangriffin@vt.edu>
2025-03-04 13:58:43 -0500

    Update to old config

--- CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
+++ CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
@@ -176,14 +176,15 @@ print("fft_size =", fft_size)
 # Pluto receive buffer size needs to be greater than total time for all chirps
 total_time = tdd.frame_length_ms * num_chirps   # time in ms
 print("Total Time for all Chirps:  ", total_time, "ms")
-buffer_time = 0
-power=12
-while total_time > buffer_time:     
-    power=power+1
-    buffer_size = int(2**power) 
-    buffer_time = buffer_size/my_sdr.sample_rate*1000   # buffer time in ms
-    if power==23:
-        break     # max pluto buffer size is 2**23, but for tdd burst mode, set to 2**22
+buffer_time = total_time + 1.0
+buffer_size = buffer_time*my_sdr.sample_rate/1000
+# power=12
+# while total_time > buffer_time:     
+#     power=power+1
+#     buffer_size = int(2**power) 
+#     buffer_time = buffer_size/my_sdr.sample_rate*1000   # buffer time in ms
+#     if power==23:
+#         break     # max pluto buffer size is 2**23, but for tdd burst mode, set to 2**22
 print("buffer_size:", buffer_size)
 my_sdr.rx_buffer_size = buffer_size
 print("buffer_time:", buffer_time, " ms")
