db262bce29c6aaf7f731f2e3d917c98869f4e98a
Nathan Griffin <nathangriffin@vt.edu>
2025-03-06 14:33:02 -0500

    full array

--- CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
+++ CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
@@ -676,7 +676,7 @@ def export_data_to_csv():
     
     for t in sorted(image_data.keys())[1:(img_size+1)]:  # Skip the first time sample
         # downsampled_data.append(downsample(image_data[t], 224))
-        downsampled_data.append(downsample(image_data[t][len(image_data[t]):], img_size))
+        downsampled_data.append(downsample(image_data[t], img_size))
         
 
     downsampled_data = np.array(downsampled_data).T
