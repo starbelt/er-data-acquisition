823d4e2d96d0c221a4879c29482b4e7d3fd47e41
Nathan Griffin <nathangriffin@vt.edu>
2025-03-06 11:32:15 -0500

    more center_freq tests

--- CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
+++ CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
@@ -32,7 +32,7 @@ import cv2 # type: ignore
 
 '''Key Parameters'''
 sample_rate = 0.682e6
-center_freq = 100e6
+center_freq = .25e9
 signal_freq = 100e3
 rx_gain = 20   # must be between -3 and 70
 output_freq = 10e9
