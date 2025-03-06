26621a1792060d297e34d47d63faceb6256d2e59
Nathan Griffin <nathangriffin@vt.edu>
2025-03-06 15:17:20 -0500

    1.3 e

--- CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
+++ CFAR/CFAR_RADAR_Waterfall_ChirpSync_Export.py
@@ -645,7 +645,7 @@ def export_data_to_csv():
     for row in data_list:
         t_since_start = float(row[0])
         frequency = float(row[1])
-        if lower_freq/1.4 < frequency < upper_freq*1.4:
+        if lower_freq/1.3 < frequency < upper_freq*1.3:
             filtered_data[t_since_start].append(row)
     first_t_start = sorted(filtered_data.keys())[0]
     num_per_sample = len(filtered_data[first_t_start])
