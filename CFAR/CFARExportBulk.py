4f817ad662569e7938a2cd66357fb2cdd4326a37
Nathan Griffin <nathangriffin@vt.edu>
2025-03-18 11:48:26 -0400

    68.5 in

--- CFAR/CFARExportBulk.py
+++ CFAR/CFARExportBulk.py
@@ -31,7 +31,7 @@ from collections import defaultdict
 import cv2 # type: ignore
 
 '''Key Parameters'''
-true_dist = 62.5 # inches
+true_dist = 68.5 # inches
 true_dist = true_dist * 2.54 / 100  # convert to meters
 
 #Class bins
